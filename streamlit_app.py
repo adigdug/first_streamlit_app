2e2ee677721b8b0959c825b8f6c06973e8a4dce5
adigdug <66643238+adigdug@users.noreply.github.com>
2023-02-26 12:30:58 +0000

    Update streamlit_app.py
    Moved the emojis inside the quotes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
-streamlit.text(🍲'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-boiled Free-Range egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
