8525d5511e4995158c694379f76edca566ced762
adigdug <66643238+adigdug@users.noreply.github.com>
2023-02-27 23:36:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error import URLERROR
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
