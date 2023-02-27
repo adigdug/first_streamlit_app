3e20edc841c730b43009fc9bfd7690d01734d3f0
adigdug <66643238+adigdug@users.noreply.github.com>
2023-02-27 23:31:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -32,7 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # the extracted data is normalized using a pandas dataframe
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
